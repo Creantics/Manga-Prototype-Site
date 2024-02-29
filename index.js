b669df2b21a00d24edf89307985afeba4d0090cc
Creantics <domsendsmail@gmail.com>
2024-02-29 13:41:01 -0500

    Initial Upload

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict"
+//Modal Elements
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.openModal');
+const closeModal = document.querySelector('.closeModal');
+//Image Elements
+let imageArray = document.querySelectorAll(".array");
+let modalImage = document.querySelector("#modalImage");
+//Button Elements
+let leftButton = document.getElementsByClassName(".left");
+let rightButton = document.getElementsByClassName(".right");
+
+//Array Itteration
+let i = 0;
+
+function left () {
+    if (i > 0 && i <= 6) {
+        i--;
+        modalImage.src = imageArray[i].src;
+        console.log(i);
+    }
+}
+
+function right () {
+    if (i >= 0 && i < 6) {
+        i++;
+        modalImage.src = imageArray[i].src;
+        console.log(i);
+    }
+}
+
+//Open/Close Modal
+openModal.addEventListener("click", () => {
+    modal.showModal();
+    modalImage.src = imageArray[0].src;
+})
+
+closeModal.addEventListener("click", () => {
+    modal.close();
+})
+
+//Testing Section
